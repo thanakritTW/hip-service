2ce44318ec64eaa3501aa6de5496d23396442431
Barney Spencer <barny.spencer@gmail.com>
2020-07-16 16:28:21 +1000

    HIP 15 | Barney, David | Adding the OpenMRS impl of the IMatchingRepository, and initial tests.

---
+++ src/In.ProjectEKA.HipService/Discovery/OpenMrsPatientMatchingRepository.cs
@@ -0,0 +1,32 @@
+namespace In.ProjectEKA.HipService.Discovery
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System;
+    using System.Threading.Tasks;
+    using HipLibrary.Matcher;
+    using HipLibrary.Patient.Model;
+    using DiscoveryRequest = HipLibrary.Patient.Model.DiscoveryRequest;
+    using static HipLibrary.Matcher.StrongMatcherFactory;
+
+    public class OpenMrsPatientMatchingRepository : IMatchingRepository
+    {
+        private readonly IPatientDal _patientDal;
+        public OpenMrsPatientMatchingRepository(IPatientDal patientDal)
+        {
+            _patientDal = patientDal;
+        }
+
+        public async Task<IQueryable<Patient>> Where(DiscoveryRequest request)
+        {
+            if(request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            var result = await _patientDal.LoadPatientsAsync(request.Patient?.Name, request.Patient?.Gender, request.Patient?.YearOfBirth);
+
+            return (from r in result select new Patient() { Name = r.Name.First().Text }).ToList().AsQueryable();
+        }
+    }
+}
---
+++ test/In.ProjectEKA.HipServiceTest/Discovery/OpenMrsPatientMatchingRepositoryTest.cs
@@ -0,0 +1,66 @@
+namespace In.ProjectEKA.HipServiceTest.Discovery
+{
+    using System;
+    using FluentAssertions;
+    using HipService.Discovery;
+    using HipService.Discovery.Database;
+    using HipLibrary.Patient.Model;
+    using Microsoft.EntityFrameworkCore;
+    using Xunit;
+    using Moq;
+    using System.Collections.Generic;
+    using FluentAssertions;
+    using System.Linq;
+    using System.Threading.Tasks;
+    public class OpenMrsPatientMatchingRepositoryTest
+    {
+        private Mock<IPatientDal> patientDal = new Mock<IPatientDal>();
+
+        public OpenMrsPatientMatchingRepositoryTest()
+        {
+                patientDal.Setup(e => e.LoadPatientsAsync(
+                It.IsAny<string>(),
+                It.IsAny<Gender?>(),
+                It.IsAny<ushort?>())).Returns( (string name, Gender? gender, ushort? yob) => {
+                    var humanName = new Hl7.Fhir.Model.HumanName();
+                    humanName.Text = name;
+                    return Task.FromResult(new List<Hl7.Fhir.Model.Patient>(){ new Hl7.Fhir.Model.Patient() { Name = new List<Hl7.Fhir.Model.HumanName>{ humanName } }});
+                }
+                );
+        }
+
+        [Fact]
+        private async void PatientDalIsInvokedWithExpectedParameters()
+        {
+            const string  patientName = "patient name";
+            Gender? patientGender = Gender.F;
+            ushort?  patientYob = 1981;
+
+            var patientEnquiry = new PatientEnquiry("id", verifiedIdentifiers: null, unverifiedIdentifiers: null, patientName, patientGender, patientYob);
+            var request = new DiscoveryRequest(patientEnquiry,"requestId", "transactionId", DateTime.Now);
+            var repo = new OpenMrsPatientMatchingRepository(patientDal.Object);
+           
+            var result = repo.Where(request);
+
+            patientDal.Verify( x => x.LoadPatientsAsync(patientName, patientGender, patientYob), Times.Once);
+
+        }
+
+        [Fact]
+        private async void ReturnsAnHIPPatientWithExpectedValues()
+        {
+            const string  patientName = "patient name";
+            Gender? patientGender = Gender.F;
+            ushort?  patientYob = 1981;
+
+            var patientEnquiry = new PatientEnquiry("id", verifiedIdentifiers: null, unverifiedIdentifiers: null, patientName, patientGender, patientYob);
+            var request = new DiscoveryRequest(patientEnquiry,"requestId", "transactionId", DateTime.Now);
+            var repo = new OpenMrsPatientMatchingRepository(patientDal.Object);
+           
+            var patient = repo.Where(request).Result.Single();
+
+            patient.Name.Should().Be(patientName);
+
+        }
+    }
+}
