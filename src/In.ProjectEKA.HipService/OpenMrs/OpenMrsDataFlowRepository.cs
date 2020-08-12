8f8dc361e560403272fe223d9f3b9f377dc53b57
Sneha Narendran <sneha.narendran@thoughtworks.com>
2020-08-12 13:16:52 +0530

    HIP#33| Get observations for visits

---
+++ src/In.ProjectEKA.HipService/OpenMrs/OpenMrsDataFlowRepository.cs
@@ -0,0 +1,75 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Web;
+using In.ProjectEKA.HipLibrary.Patient;
+using In.ProjectEKA.HipLibrary.Patient.Model;
+using In.ProjectEKA.HipService.Logger;
+
+namespace In.ProjectEKA.HipService.OpenMrs
+{
+    public class OpenMrsDataFlowRepository : IOpenMrsDataFlowRepository
+    {
+        private readonly IOpenMrsClient openMrsClient;
+        public OpenMrsDataFlowRepository(IOpenMrsClient openMrsClient)
+        {
+            this.openMrsClient = openMrsClient;
+        }
+
+        public async Task<List<Observation>> LoadObservationsForVisits(string patientReferenceNumber, string visitTypeDisplay)
+        {
+            var path = DataFlowPathConstants.OnVisitPath;
+            var query = HttpUtility.ParseQueryString(string.Empty);
+            if (!string.IsNullOrEmpty(patientReferenceNumber))
+            {
+                query["patient"] = patientReferenceNumber;
+                query["v"] = "full";
+            }
+            if (query.ToString() != "")
+            {
+                path = $"{path}?{query}";
+            }
+
+            var response = await openMrsClient.GetAsync(path);
+            var content = await response.Content.ReadAsStringAsync();
+
+            var jsonDoc = JsonDocument.Parse(content);
+            var root = jsonDoc.RootElement;
+
+            var observations = new List<Observation>();
+            var results = root.GetProperty("results");
+            for (int i = 0; i < results.GetArrayLength(); i++)
+            {
+                var visitType = results[i].GetProperty("visitType");
+                var display = visitType.GetProperty("display").GetString();
+                if (display == visitTypeDisplay)
+                {
+                    var encounters = results[i].GetProperty("encounters");
+                    if (encounters.GetArrayLength() != 0)
+                    {
+                        for (int j = 0; j < encounters.GetArrayLength(); j++)
+                        {
+                            var obs = encounters[j].GetProperty("obs");
+                            if (obs.GetArrayLength() != 0)
+                            {
+                                for (int k = 0; k < obs.GetArrayLength(); k++)
+                                {
+                                    observations.Add(new Observation(obs[k].GetProperty("uuid").ToString(), obs[k].GetProperty("display").ToString()));
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+
+
+            return observations;
+        }
+
+
+    }
+}
