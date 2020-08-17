5fe820d8f0dd7b6e5f01f1312c997131a9776362
Sneha Narendran <sneha.narendran@thoughtworks.com>
2020-08-17 12:03:39 +0530

    HIP#33 Get diagnosis report for visits

--- src/In.ProjectEKA.HipLibrary/Patient/Model/Diagnosis.cs
+++ src/In.ProjectEKA.HipLibrary/Patient/Model/Diagnosis.cs
@@ -1,15 +1,15 @@
 namespace In.ProjectEKA.HipLibrary.Patient.Model
 {
-  public class Diagnosis
-  {
-    public string ReferenceNumber { get; }
-
-    public string Display { get; }
-
-    public Diagnosis(string referenceNumber, string display)
+    public class Diagnosis
     {
-      ReferenceNumber = referenceNumber;
-      Display = display;
+        public string ReferenceNumber { get; }
+
+        public string Display { get; }
+
+        public Diagnosis(string referenceNumber, string display)
+        {
+            ReferenceNumber = referenceNumber;
+            Display = display;
+        }
     }
-  }
 }
