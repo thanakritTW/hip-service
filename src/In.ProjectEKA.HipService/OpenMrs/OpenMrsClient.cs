ce264daf2b2ef58956c575cde2b60376c4d7fb75
Gwan Thanakrit <thanakrit.j@thoughtworks.com>
2020-08-17 10:29:11 +0700

    HIP #60 | Gwan | Add error logs and throw error when getting non success status code from openmrs

--- src/In.ProjectEKA.HipService/OpenMrs/OpenMrsClient.cs
+++ src/In.ProjectEKA.HipService/OpenMrs/OpenMrsClient.cs
@@ -22,15 +22,25 @@ namespace In.ProjectEKA.HipService.OpenMrs
 
         public async Task<HttpResponseMessage> GetAsync(string openMrsUrl)
         {
+            HttpResponseMessage responseMessage;
             try
             {
-                return await httpClient.GetAsync(Path.Join(configuration.Url, openMrsUrl));
+                responseMessage = await httpClient.GetAsync(Path.Join(configuration.Url, openMrsUrl));
             }
             catch (Exception exception)
             {
                 Log.Error(exception, exception.StackTrace);
                 throw exception;
             }
+
+            if (!responseMessage.IsSuccessStatusCode)
+            {
+                var error = await responseMessage.Content.ReadAsStringAsync();
+                Log.Error(
+                    $"Failure in getting the data from OpenMrs with status code {responseMessage.StatusCode} {error}");
+                throw new OpenMrsNetworkException();
+            }
+            return responseMessage;
         }
 
         private void SettingUpHeaderAuthorization()
