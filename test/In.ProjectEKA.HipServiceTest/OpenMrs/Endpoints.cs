d740e2e58a0d66bc8e4a303809e20fe19e667bd5
Sneha Narendran <sneha.narendran@thoughtworks.com>
2020-08-17 18:47:14 +0530

    HIP#33 Get conditions for visits

--- test/In.ProjectEKA.HipServiceTest/OpenMrs/Endpoints.cs
+++ test/In.ProjectEKA.HipServiceTest/OpenMrs/Endpoints.cs
@@ -4,18 +4,18 @@ namespace In.ProjectEKA.HipServiceTest.OpenMrs
   {
     public static class OpenMrs
     {
-      public const string OnProgramEnrollmentPath = "ws/rest/v1/bahmniprogramenrollment";
-      public const string OnVisitPath = "ws/rest/v1/visit";
-      public const string OnPatientPath = "ws/rest/v1/patient";
+        public const string OnProgramEnrollmentPath = "ws/rest/v1/bahmniprogramenrollment";
+        public const string OnVisitPath = "ws/rest/v1/visit";
+        public const string OnPatientPath = "ws/rest/v1/patient";
     }
 
     public static class Fhir
     {
-      public const string OnPatientPath = "ws/fhir2/Patient";
+        public const string OnPatientPath = "ws/fhir2/Patient";
     }
     public static class EMRAPI
     {
-      public const string OnConditionPath = "ws/rest/emrapi/conditionhistory";
+        public const string OnConditionPath = "ws/rest/emrapi/conditionhistory";
     }
   }
 }
