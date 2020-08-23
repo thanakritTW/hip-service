c8d16806107adeddeec7d21efef12b0f38c2843a
Cecily Shen <cecily.shen@thoughtworks.com>
2020-08-24 09:29:16 +1000

    Added missing space

--- src/In.ProjectEKA.HipService/OpenMrs/DataFlowPathConstants.cs
+++ src/In.ProjectEKA.HipService/OpenMrs/DataFlowPathConstants.cs
@@ -3,6 +3,6 @@ namespace In.ProjectEKA.HipService.OpenMrs
     public static class DataFlowPathConstants
     {
         public const string OnVisitPath = "ws/rest/v1/visit";
-        public const string OnConditionPath="ws/rest/emrapi/conditionhistory";
+        public const string OnConditionPath= "ws/rest/emrapi/conditionhistory";
     }
 }
