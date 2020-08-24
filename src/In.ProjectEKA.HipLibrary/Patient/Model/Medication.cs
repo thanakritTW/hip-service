42964e4b6658737fb71d3a76a3905c4277066357
Cecily Shen <cecily.shen@thoughtworks.com>
2020-08-24 11:31:06 +1000

    Adding final new lines

--- src/In.ProjectEKA.HipLibrary/Patient/Model/Medication.cs
+++ src/In.ProjectEKA.HipLibrary/Patient/Model/Medication.cs
@@ -1,18 +1,17 @@
 namespace In.ProjectEKA.HipLibrary.Patient.Model
 {
-    public class Medication
+  public class Medication
+  {
+    public string ReferenceNumber { get; }
+
+    public string Display { get; }
+
+    public string Type { get; }
+
+    public Medication(string referenceNumber, string display, string type)
     {
-        public string ReferenceNumber { get; }
-
-        public string Display { get; }
-
-        public string Type { get; }
-
-        public Medication(string referenceNumber, string display, string type)
-        {
-            ReferenceNumber = referenceNumber;
-            Display = display;
-            Type = type;
-        }
+      ReferenceNumber = referenceNumber;
+      Display = display;
+      Type = type;
     }
 }
--- src/In.ProjectEKA.HipLibrary/Patient/Model/Observation.cs
+++ src/In.ProjectEKA.HipLibrary/Patient/Model/Observation.cs
@@ -1,15 +1,14 @@
 namespace In.ProjectEKA.HipLibrary.Patient.Model
 {
-    public class Observation
+  public class Observation
+  {
+    public string ReferenceNumber { get; }
+
+    public string Display { get; }
+
+    public Observation(string referenceNumber, string display)
     {
-        public string ReferenceNumber { get; }
-
-        public string Display { get; }
-
-        public Observation(string referenceNumber, string display)
-        {
-            ReferenceNumber = referenceNumber;
-            Display = display;
-        }
+      ReferenceNumber = referenceNumber;
+      Display = display;
     }
 }
