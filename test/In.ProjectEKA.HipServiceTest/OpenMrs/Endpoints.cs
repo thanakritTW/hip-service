ee30a53d02602a8060dc8bdca09dafea73a8eb39
Cecily Shen <cecily.shen@thoughtworks.com>
2020-08-26 10:47:25 +1000

    Fix formatting

--- test/In.ProjectEKA.HipServiceTest/OpenMrs/Endpoints.cs
+++ test/In.ProjectEKA.HipServiceTest/OpenMrs/Endpoints.cs
@@ -1,21 +1,21 @@
 namespace In.ProjectEKA.HipServiceTest.OpenMrs
 {
-  public static class Endpoints
-  {
-    public static class OpenMrs
+    public static class Endpoints
     {
-      public const string OnProgramEnrollmentPath = "ws/rest/v1/bahmniprogramenrollment";
-      public const string OnVisitPath = "ws/rest/v1/visit";
-      public const string OnPatientPath = "ws/rest/v1/patient";
-    }
+        public static class OpenMrs
+        {
+        public const string OnProgramEnrollmentPath = "ws/rest/v1/bahmniprogramenrollment";
+        public const string OnVisitPath = "ws/rest/v1/visit";
+        public const string OnPatientPath = "ws/rest/v1/patient";
+        }
 
-    public static class Fhir
-    {
-      public const string OnPatientPath = "ws/fhir2/Patient";
+        public static class Fhir
+        {
+        public const string OnPatientPath = "ws/fhir2/Patient";
+        }
+        public static class EMRAPI
+        {
+        public const string OnConditionPath = "ws/rest/emrapi/conditionhistory";
+        }
     }
-    public static class EMRAPI
-    {
-      public const string OnConditionPath = "ws/rest/emrapi/conditionhistory";
-    }
-  }
 }
